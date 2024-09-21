d614efab94d71dfaa7fed029203a719a58ae0763
TASDevelopers <thearcsoldevelopers@gmail.com>
2024-09-21 23:21:48 +0530

    Deployed

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import ghPages from 'vite-plugin-gh-pages';
+// vite.config.js
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/Package-Helper/', 
-  plugins: [ghPages()]
-})
+});
